f0e2150b996c534c4a3f077e1b3551aa368bebbf
heimaxiaobo <zpjinbo@163.com>
2025-03-10 15:33:35 +0800

    Update build_all.sh
    修改成动态库openssl

--- build_all.sh
+++ build_all.sh
@@ -19,7 +19,7 @@ function build_openssl() {
     INSTALL_DIR=${BUILD_DIR}/openssl-${OPENSSL_VERSION}/${ANDROID_ABI}
     mkdir -p ${INSTALL_DIR}
     
-    ./Configure ${OPENSSL_ARCH} no-tests no-unit-test no-shared -static no-asm -D__ANDROID_API__=${MIN_API} --prefix=${INSTALL_DIR} -fPIC
+    ./Configure ${OPENSSL_ARCH} no-tests no-unit-test shared -D__ANDROID_API__=${MIN_API} --prefix=${INSTALL_DIR} -fPIC
     make -j$(($(getconf _NPROCESSORS_ONLN) + 1))
     make install_sw
     #clean up
