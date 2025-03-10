65f4d5b2cc521fff1a5144edd8cf8645080c3715
heimaxiaobo <zpjinbo@163.com>
2025-03-11 00:14:45 +0800

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -8,6 +8,10 @@ BUILD_DIR=${WORK_PATH}/build
 OPENSSL_SRC_DIR=${WORK_PATH}/openssl-${OPENSSL_VERSION}
 CURL_SRC_DIR=${WORK_PATH}/curl-${CURL_VERSION}
 
+# 设置HTTP/3相关源码目录
+NGHTTP3_SRC_DIR=${WORK_PATH}/nghttp3-${NGHTTP3_VERSION}
+NGTCP2_SRC_DIR=${WORK_PATH}/ngtcp2-${NGTCP2_VERSION}
+
 export ANDROID_NDK_ROOT=${WORK_PATH}/android-ndk-${ANDROID_NDK_VERSION}
 TOOLCHAIN=${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/${HOST_TAG}
 PATH=${TOOLCHAIN}/bin:$PATH
@@ -19,13 +23,14 @@ function build_openssl() {
     INSTALL_DIR=${BUILD_DIR}/openssl-${OPENSSL_VERSION}/${ANDROID_ABI}
     mkdir -p ${INSTALL_DIR}
     
-    # 添加更多no-xxx选项和编译优化选项以减小libcrypto.so大小
-    ./Configure ${OPENSSL_ARCH} no-tests no-unit-test shared -D__ANDROID_API__=${MIN_API} --prefix=${INSTALL_DIR} -fPIC
-    -ffunction-sections -fdata-sections
+    # 修改OpenSSL编译选项，保留HTTP/3所需的加密算法
+    ./Configure ${OPENSSL_ARCH} no-tests no-unit-test shared -D__ANDROID_API__=${MIN_API} --prefix=${INSTALL_DIR} -fPIC \
+    -ffunction-sections -fdata-sections \
+    enable-tls1_3 enable-ec enable-ecdh enable-ecdsa
+    
     make -j$(($(getconf _NPROCESSORS_ONLN) + 1))
     make install_sw
     #clean up
-    rm -rf ${OPENSSL_SRC_DIR}
     rm -rf ${INSTALL_DIR}/bin
     rm -rf ${INSTALL_DIR}/share
     rm -rf ${INSTALL_DIR}/ssl
@@ -33,8 +38,71 @@ function build_openssl() {
     rm -rf ${INSTALL_DIR}/lib/pkgconfig
     # Keep dynamic libraries but remove unnecessary files
     rm -rf ${INSTALL_DIR}/lib/ossl-modules
+}
 
+function build_nghttp3() {
+    export TARGET_HOST=$1
+    export ANDROID_ARCH=${ANDROID_ABI}
+    export AR=${TOOLCHAIN}/bin/llvm-ar
+    export CC=${TOOLCHAIN}/bin/${TARGET_HOST}${MIN_API}-clang
+    export AS=${CC}
+    export CXX=${TOOLCHAIN}/bin/${TARGET_HOST}${MIN_API}-clang++
+    export LD=${TOOLCHAIN}/bin/ld
+    export RANLIB=${TOOLCHAIN}/bin/llvm-ranlib
+    export STRIP=${TOOLCHAIN}/bin/llvm-strip
     
+    INSTALL_DIR=${BUILD_DIR}/nghttp3-${NGHTTP3_VERSION}/${ANDROID_ABI}
+    mkdir -p ${INSTALL_DIR}
+    
+    # 配置nghttp3
+    autoreconf -fi
+    ./configure --host=${TARGET_HOST} \
+                --target=${TARGET_HOST} \
+                --prefix=${INSTALL_DIR} \
+                --enable-lib-only \
+                --disable-shared \
+                --enable-static
+    
+    make -j$(($(getconf _NPROCESSORS_ONLN) + 1))
+    make install
+    
+    # 清理
+    rm -rf ${INSTALL_DIR}/bin
+    rm -rf ${INSTALL_DIR}/share
+    rm -rf ${INSTALL_DIR}/lib/pkgconfig
+}
+
+function build_ngtcp2() {
+    export TARGET_HOST=$1
+    export ANDROID_ARCH=${ANDROID_ABI}
+    export AR=${TOOLCHAIN}/bin/llvm-ar
+    export CC=${TOOLCHAIN}/bin/${TARGET_HOST}${MIN_API}-clang
+    export AS=${CC}
+    export CXX=${TOOLCHAIN}/bin/${TARGET_HOST}${MIN_API}-clang++
+    export LD=${TOOLCHAIN}/bin/ld
+    export RANLIB=${TOOLCHAIN}/bin/llvm-ranlib
+    export STRIP=${TOOLCHAIN}/bin/llvm-strip
+    
+    INSTALL_DIR=${BUILD_DIR}/ngtcp2-${NGTCP2_VERSION}/${ANDROID_ABI}
+    mkdir -p ${INSTALL_DIR}
+    
+    # 配置ngtcp2
+    autoreconf -fi
+    ./configure --host=${TARGET_HOST} \
+                --target=${TARGET_HOST} \
+                --prefix=${INSTALL_DIR} \
+                --with-openssl=${BUILD_DIR}/openssl-${OPENSSL_VERSION}/${ANDROID_ABI} \
+                --enable-lib-only \
+                --disable-shared \
+                --enable-static
+    
+    make -j$(($(getconf _NPROCESSORS_ONLN) + 1))
+    make install
+    
+    # 清理
+    rm -rf ${INSTALL_DIR}/bin
+    rm -rf ${INSTALL_DIR}/share
+    rm -rf ${INSTALL_DIR}/lib/pkgconfig
 }
 
 function build_curl() {
@@ -51,11 +119,15 @@ function build_curl() {
     INSTALL_DIR=${BUILD_DIR}/curl-${CURL_VERSION}/${ANDROID_ABI}
     mkdir -p ${INSTALL_DIR}
     
+    # 添加HTTP/3支持的配置选项
     ./configure --host=${TARGET_HOST} \
                 --target=${TARGET_HOST} \
                 --prefix=${INSTALL_DIR} \
                 --with-openssl=${BUILD_DIR}/openssl-${OPENSSL_VERSION}/${ANDROID_ABI} \
+                --with-nghttp3=${BUILD_DIR}/nghttp3-${NGHTTP3_VERSION}/${ANDROID_ABI} \
+                --with-ngtcp2=${BUILD_DIR}/ngtcp2-${NGTCP2_VERSION}/${ANDROID_ABI} \
                 --with-pic --enable-ipv6 --enable-http2 \
+                --enable-alt-svc \
                 --disable-ldap --disable-ldaps --disable-manual --disable-libcurl-option \
                 --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 \
                 --disable-imap --disable-smtp --disable-gopher --disable-smb \
@@ -63,25 +135,29 @@ function build_curl() {
                 --disable-verbose --disable-versioned-symbols \
                 --disable-ftp --disable-file --disable-netrc --disable-fsck-zero-pct \
                 --without-brotli --without-zlib --without-zstd --without-libidn2 \
-                --without-nghttp2 --without-librtmp --without-libpsl \
+                --without-librtmp --without-libpsl \
                 --enable-shared --disable-static
 
     make -j$(($(getconf _NPROCESSORS_ONLN) + 1))
     make install
     #clean up
-    rm -rf ${CURL_SRC_DIR}
     rm -rf ${INSTALL_DIR}/bin
     rm -rf ${INSTALL_DIR}/share
     rm -rf ${INSTALL_DIR}/lib/pkgconfig
 }
 
 
-
-
 if [ "$ANDROID_ABI" == "armeabi-v7a" ]
 then
     cd ${OPENSSL_SRC_DIR}
     build_openssl armv7a-linux-androideabi android-arm
+    
+    cd ${NGHTTP3_SRC_DIR}
+    build_nghttp3 armv7a-linux-androideabi
+    
+    cd ${NGTCP2_SRC_DIR}
+    build_ngtcp2 armv7a-linux-androideabi
+    
     cd ${CURL_SRC_DIR}
     build_curl armv7a-linux-androideabi
     
@@ -89,6 +165,13 @@ elif [ "$ANDROID_ABI" == "arm64-v8a" ]
 then
     cd ${OPENSSL_SRC_DIR}
     build_openssl aarch64-linux-android android-arm64
+    
+    cd ${NGHTTP3_SRC_DIR}
+    build_nghttp3 aarch64-linux-android
+    
+    cd ${NGTCP2_SRC_DIR}
+    build_ngtcp2 aarch64-linux-android
+    
     cd ${CURL_SRC_DIR}
     build_curl aarch64-linux-android
 else
